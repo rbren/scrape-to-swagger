77a36e4bd99679a1dd2fae8c8c9715fb18f2458a
Ubuntu <ubuntu@ip-10-0-0-252.us-west-2.compute.internal>
2015-12-17 15:47:07 +0000

    add depth parsing

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+var urlParser = require('url');
 var fs = require('fs');
+var async = require('async');
 var request = require('request');
 var cheerio = require('cheerio');
 var argv = require('yargs').argv;
@@ -6,11 +8,33 @@ var config = require(argv.config);
 
 var swagger = {swagger: '2.0'};
 
-request.get(config.url, function(err, resp, body) {
-  var $ = cheerio.load(body);
-  for (var field in config.fields) {
-    var selector = config.fields[field].selector;
-    swagger[field] = $(selector).text();
-  }
+var parsed = urlParser.parse(config.url);
+var host = parsed.protocol + '//' + parsed.host;
+
+function scrapePage(url, depth, callback) {
+  console.log('resolve', host, url);
+  url = urlParser.resolve(host, url);
+  console.log('scrape', url);
+  request.get(url, function(err, resp, body) {
+    if (err) return callback(err);
+    var $ = cheerio.load(body);
+    for (var field in config.fields) {
+      var selector = config.fields[field].selector;
+      swagger[field] = $(selector).text();
+    }
+    if (!depth) return callback();
+    var links = $('a[href]');
+    async.parallel($('a[href]').map(function(i, el) {
+      return function(acb) {
+        scrapePage($(el).attr('href'), depth - 1, acb);
+      }
+    }), function(err) {
+      callback(err);
+    })
+  })
+}
+
+scrapePage(config.url, config.depth || 1, function(err) {
+  if (err) throw err;
   fs.writeFileSync(argv.output || 'swagger.json', JSON.stringify(swagger, null, 2));
-})
+});
