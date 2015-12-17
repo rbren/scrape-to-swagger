2b68cb5f2bc63cf2bae1fbb570d2c326b4a59fc8
Ubuntu <ubuntu@ip-10-0-0-252.us-west-2.compute.internal>
2015-12-17 15:32:44 +0000

    basics working

---
+++ config/github.js
@@ -0,0 +1,6 @@
+var config = module.exports = {
+  url: 'https://developer.github.com/v3/',
+  fields: {
+    title: {selector: 'h2'},
+  },
+}
--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+var fs = require('fs');
 var request = require('request');
 var cheerio = require('cheerio');
 var argv = require('yargs').argv;
@@ -5,11 +6,11 @@ var config = require(argv.config);
 
 var swagger = {swagger: '2.0'};
 
-request.get(argv.url, function(err, resp, body) {
+request.get(config.url, function(err, resp, body) {
   var $ = cheerio.load(body);
   for (var field in config.fields) {
     var selector = config.fields[field].selector;
     swagger[field] = $(selector).text();
   }
-  FS.writeFileSync(argv.output || 'swagger.json', JSON.stringify(swaggger, null, 2));
+  fs.writeFileSync(argv.output || 'swagger.json', JSON.stringify(swagger, null, 2));
 })
