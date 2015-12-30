394354d9a0f0ec42e4ce1bf8be8cfdc280543e99
Ubuntu <ubuntu@ip-10-0-0-252.us-west-2.compute.internal>
2015-12-30 20:11:59 +0000

    start adding quandl

---
+++ config/quandl.js
@@ -0,0 +1,32 @@
+var config = module.exports = {
+  url: 'https://www.quandl.com/docs/api',
+  depth: 0,
+  protocols: ['https'],
+  host: 'www.quandl.com',
+  basePath: '/api/v3',
+  title: 'Quandl API',
+  description: 'The Quandl API',
+
+  operation: {selector: 'h2', split: true},
+  path: {selector: 'pre:contains(Definition) + blockquote', regex: /\w+ (\/\S*)/, sibling: true},
+  method: {selector: 'pre:contains(Definition) + blockquote', regex: /(\w+) \/\S*/, sibling: true},
+
+  parameters: {selector: 'table', sibling: true},
+  parameter: {selector: 'tr'},
+  parameterName: {selector: 'td:first-of-type', regex: /(\S+)/},
+  parameterType: {selector: 'td:nth-of-type(2)'},
+  parameterDescription: {selector: 'td:nth-of-type(3)'},
+  requestBody: {selector: 'h4:contains(Example) ~ pre.highlight-json, h3:contains(Example) ~ pre.highlight-json', isExample: true, sibling: true},
+  responses: {selector: 'h3:contains(Response), h4:contains(Response)', sibling: true},
+  responseStatus: {selector: 'pre.highlight-headers', regex: /Status: (\d+) /, sibling: true},
+  responseDescription: {selector: 'pre.highlight-headers', regex: /Status: \d+ (.*)/, sibling: true},
+  responseSchema: {selector: 'pre.highlight-json', isExample: true, sibling: true},
+
+  fixPathParameters: function(path) {
+    pieces = path.split('/');
+    pieces = pieces.map(function(p) {
+      return p.replace(/:(\w+)/g, '{$1}')
+    })
+    return pieces.join('/');
+  }
+}
